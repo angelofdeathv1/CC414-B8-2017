25e893b81b73d62c61947728e4d3b8c709bb9834
Angel Arambula Garcia <aarambula@bajaware.com>
2017-04-27 16:57:13 -0700

    C# example

---
+++ ConsoleApplication1/ConsoleApplication1/DBUtils.cs
@@ -0,0 +1,23 @@
+﻿using Oracle.DataAccess.Client;
+using System;
+namespace ConsoleApplication1
+{
+    public class DBUtils
+    {
+        OracleConnection con;
+        public void Connect()
+        {
+            con = new OracleConnection();
+            con.ConnectionString = "User Id=examen02;Password=examen02;Data Source=(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=127.0.0.1)(PORT=1521))(CONNECT_DATA=(SID=XE)));";
+            con.Open();
+            Console.WriteLine("Connected to Oracle" + con.ServerVersion);
+        }
+
+        public void Close()
+        {
+            con.Close();
+            con.Dispose();
+        }
+
+    }
+}
